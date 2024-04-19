ac3fe4ad002cbde003246168183f38c2885d93a9
Kenneth5817 <148528692+Kenneth5817@users.noreply.github.com>
2024-04-19 17:53:24 +0200

    Update PointSpec.java

--- src/test/java/org/iesvdm/tddjava/ship/PointSpec.java
+++ src/test/java/org/iesvdm/tddjava/ship/PointSpec.java
@@ -1,94 +1,33 @@
 package org.iesvdm.tddjava.ship;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
-import org.testng.Assert;
+
+import org.testng.annotations.*;
 
 import java.util.ArrayList;
 import java.util.List;
+
 import static org.testng.Assert.*;
 
 
+
+@Test
 public class PointSpec {
-    private Ship ship;
-    private Location location;
-    private Planet planet;
+    private Point point;
+    private final int x = 12;
+    private final int y = 21;
+
     public PointSpec() {
     }
-    @BeforeEach
+
+    @BeforeMethod
     public void beforeTest() {
-        Point max = new Point(50, 50);
-        this.location = new Location(new Point(21, 13), Direction.NORTH);
-        List<Point> obstacles = new ArrayList<>();
-        obstacles.add(new Point(44, 44));
-        obstacles.add(new Point(45, 46));
-        this.planet = new Planet(max, obstacles);
-        this.ship = new Ship(this.location, this.planet);
-    }
-    @Test
-    public void whenInstantiatedThenLocationIsSet() {
-        assertEquals(location, ship.getLocation());
-    }
-    @Test
-    public void whenMoveForwardThenForward() {
-        ship.moveForward();
-        assertEquals(12, location.getPoint().getY());
-    }
-    @Test
-    public void whenMoveBackwardThenBackward() {
-        ship.moveBackward();
-        assertEquals(12, location.getPoint().getY());
-    }
-    @Test
-    public void whenTurnLeftThenLeft() {
-        ship.turnLeft();
-        assertEquals(Direction.WEST, location.getDirection());
-    }
-    @Test
-    public void whenTurnRightThenRight() {
-        ship.turnRight();
-        assertEquals(Direction.EAST, location.getDirection());
-    }
-    @Test
-    public void whenReceiveCommandsFThenForward() {
-        assertEquals("O", ship.receiveCommands("f"));
-    }
-    @Test
-    public void whenReceiveCommandsBThenBackward(){
-        assertEquals("O", ship.receiveCommands("b"));
-    }
-    @Test
-    public void whenReceiveCommandsLThenTurnLeft() {
-        assertEquals("", ship.receiveCommands("l"));
+        this.point = new Point(x, y);
     }
 
-    //Este test no me lo coge bien
-    @Test
-    public void whenReceiveCommandsRThenTurnRight() {
-        assertEquals("0", ship.receiveCommands("r"));
+    public void whenInstantiatedThenXIsSet() {
+        assertEquals(x, point.getX());
     }
 
-    @Test
-    public void whenReceiveCommandsThenAllAreExecuted() {
-        assertEquals("OOO", ship.receiveCommands("ff"));
-    }
-    @Test
-    public void whenInstantiatedThenPlanetIsStored() {
-        assertEquals(planet, ship.getPlanet());
-    }
-    @Test
-    public void givenDirectionEAndXEqualsMaxXWhenReceiveCommandsFThenWrap() {
-        assertEquals("O", ship.receiveCommands("f"));
-    }
-    @Test
-    public void givenDirectionEAndXEquals1WhenReceiveCommandsBThenWrap() {
-        assertEquals("O", ship.receiveCommands("b"));
-    }
-    @Test
-    public void whenReceiveCommandsThenStopOnObstacle() {
-        assertEquals("OX", ship.receiveCommands("ff"));
-    }
-    @Test
-    public void whenReceiveCommandsThenOForOkAndXForObstacle() {
-        assertEquals("OOX", ship.receiveCommands("ffb"));
+    public void whenInstantiatedThenYIsSet() {
+        assertEquals(y, point.getY());
     }
 }
